98459ad60635bb6a630d97c851e719440d8e0e7a
Pinki Paul <pinkipaul371@gmail.com>
2024-01-24 12:31:47 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css";
+
+
+function Footer(){
+    return(
+        
+            <p className="footer">Copyright Ⓒ {new Date().getFullYear()}</p>
+      
+    )
+}
+
+export default Footer;
